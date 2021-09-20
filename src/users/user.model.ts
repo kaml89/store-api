8e231ca4f5922696668addcd5a99ade558007d7d
kaml89 <rba.kamil@gmail.com>
2021-09-20 21:59:21 +0200

    create user model

---
+++ src/users/user.model.ts
@@ -0,0 +1,20 @@
+import { IBaseUser, IUser } from "./user.interface";
+import mongoose, { Schema, Model } from "mongoose";
+
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+UserSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const User = mongoose.model<IBaseUser>("User", UserSchema);
+
+export default User;
