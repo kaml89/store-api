01f55c8711acea8dc766ddfa1320bcbd3ea51f70
kaml89 <rba.kamil@gmail.com>
2021-09-20 21:54:36 +0200

    connect to mongodb and add usersRouter

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,36 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import mongoose from "mongoose";
+
+import { usersRouter } from "./users/user.router";
 
 dotenv.config();
 
 if (!process.env.PORT) {
-    process.exit(1);
- }
- 
- const PORT: number = parseInt(process.env.PORT as string, 10);
- const app = express();
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const MONGODB_URI = process.env.MONGODB_URI as string;
+const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", usersRouter);
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
+
+mongoose.connect(MONGODB_URI).then(
+  () => {
+    console.log("Connected to database");
+  },
+
+  (err) => {
+    console.log("Failed to connect to database");
+  }
+);
