d26b8a0751f1fbea1e993d48a2c2a7f4f482a238
kamil <rba.kamil@gmail.com>
2021-11-18 15:55:40 +0100

    add createToken function

---
+++ src/utils/utils.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { IUser } from "@users/user.interface";
+
+export const createToken = (user: IUser) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.SECRET,
+    { algorithm: "HS256", expiresIn: "30d" }
+  );
+};
