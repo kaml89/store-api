e85edc8a7563a9fb2ff05a9fccdbd4cf7a7e93ee
kamil <rba.kamil@gmail.com>
2021-11-18 15:55:08 +0100

    delete password in toJSON method

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -17,6 +17,7 @@ UserSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.password;
   },
 });
 
