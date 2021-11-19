f1eab6b4066ef6c929b619c942eeee197808cfe2
kamil <rba.kamil@gmail.com>
2021-11-19 21:16:17 +0100

    add checkRoles middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "express-jwt";
+import jwtDecode from "jwt-decode";
 import * as dotenv from "dotenv";
+import * as UserService from "../users/user.service";
+import { IUser } from "../users/user.interface";
 
 dotenv.config();
 
@@ -9,14 +12,34 @@ export const tokenExtractor = (
   res: Response,
   next: NextFunction
 ) => {
-  const authorization = req.get("authorization");
-  if (authorization) {
-    req.token = authorization.substring(7);
+  const token = req.get("authorization");
+  if (token) {
+    req.user = jwtDecode(token.substring(7));
+    return next();
   }
-  next();
+  return next();
 };
 
 export const requireAuth = jwt({
   secret: process.env.SECRET,
   algorithms: ["HS256"],
 });
+
+export const checkRoles = (roles: Array<string>) => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const id: string = req.user.id;
+    const user: IUser | null = await UserService.getById(id);
+    if (!user) {
+      res.status(401).send("Unauthorized user");
+    }
+    if (roles.indexOf(req.user.role) > -1) {
+      return next();
+    } else {
+      res.status(401).send("Unauthorized user");
+    }
+  };
+};
