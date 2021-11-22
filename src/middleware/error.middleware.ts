17e3032b0741afb034eeff9369fa12f83970ed29
kamil <rba.kamil@gmail.com>
2021-11-22 15:02:18 +0100

    add validationError handling

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,23 +1,31 @@
-import HttpException from "../common/http-exception";
+import { ApplicationError } from "../common/http-exception";
 import { Request, Response, NextFunction } from "express";
 import { ValidationError } from "express-json-validator-middleware";
 
 export const errorHandler = (
-  error: HttpException,
-  request: Request,
-  response: Response,
+  error: ApplicationError,
+  req: Request,
+  res: Response,
   next: NextFunction
 ) => {
-  const isValidationError = error instanceof ValidationError;
-  if (!isValidationError) {
-    return next(error);
+  if (error.name === "UnauthorizedError") {
+    res.status(401).send("Invalid token");
   }
 
-  response.status(400).json({
-    errors: error.validationErrors,
-  });
+  const isValidationError = error instanceof ValidationError;
+  if (isValidationError) {
+    res.status(400).json({
+      errors: error.validationErrors,
+    });
+  }
+
+  if (error.name === "ValidationError") {
+    res.status(400).json(error);
+  }
 
   const status = error.statusCode || error.status || 500;
 
-  response.status(status).send(error);
+  console.error(error);
+
+  res.status(status).send(error);
 };
