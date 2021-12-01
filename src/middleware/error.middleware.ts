3f58b3c31d9e0bb44594db300384c45b8ea9148f
kamil <rba.kamil@gmail.com>
2021-12-01 22:16:23 +0100

    refactor error middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,31 +1,34 @@
 import { ApplicationError } from "../common/http-exception";
 import { Request, Response, NextFunction } from "express";
 import { ValidationError } from "express-json-validator-middleware";
+import { MongoError } from "mongodb";
 
 export const errorHandler = (
-  error: ApplicationError,
+  error: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   if (error.name === "UnauthorizedError") {
-    return res.status(401).send("Invalid token");
+    return res.status(401).json(error);
   }
 
-  const isValidationError = error instanceof ValidationError;
-  if (isValidationError) {
+  if (error instanceof ValidationError) {
     return res.status(400).json({
       errors: error.validationErrors,
     });
   }
 
   if (error.name === "ValidationError") {
-    return res.status(400).json(error);
+    return res.status(400).json(error.message);
   }
 
-  const status = error.statusCode || error.status || 500;
+  if (error instanceof ApplicationError) {
+    console.error(error);
+    return res.status(error.statusCode).send(error.message);
+  }
 
   console.error(error);
 
-  res.status(status).send(error);
+  res.status(500).send(error);
 };
